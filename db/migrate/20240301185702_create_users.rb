3ce8bf4c0fcd21b89471384893a4f59bd7704b83
Peregrine Balas <peregrinereedbalas@gmail.com>
2024-03-01 14:07:55 -0600

    more models

--- db/migrate/20240301185702_create_users.rb
+++ db/migrate/20240301185702_create_users.rb
@@ -1,8 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
+      t.references :entity, null: false, foreign_key: true
       t.string :email
-
+      
       t.timestamps
     end
   end
