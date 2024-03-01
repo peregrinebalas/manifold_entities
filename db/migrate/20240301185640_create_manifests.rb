726e5c97ca9ab342145a81cf110cec1d8f4828f8
Peregrine Balas <peregrinereedbalas@gmail.com>
2024-03-01 14:40:04 -0600

    update fields

--- db/migrate/20240301185640_create_manifests.rb
+++ db/migrate/20240301185640_create_manifests.rb
@@ -1,7 +1,9 @@
 class CreateManifests < ActiveRecord::Migration[7.1]
   def change
     create_table :manifests do |t|
-
+      t.references :entity, null: false, foreign_key: true
+      t.string :color
+      # add vector for poly
       t.timestamps
     end
   end
--- db/migrate/20240301190002_create_portals.rb
+++ db/migrate/20240301190002_create_portals.rb
@@ -1,7 +1,8 @@
 class CreatePortals < ActiveRecord::Migration[7.1]
   def change
     create_table :portals do |t|
-
+      t.boolean :public
+      t.references :user, null: true, foreign_key: true
       t.timestamps
     end
   end
