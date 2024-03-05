44377a2d2ac0dfb202155b13c976b5815b4c7fa8
Peregrine Balas <peregrinereedbalas@gmail.com>
2024-03-04 23:18:24 -0600

    models/migrations

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -3,4 +3,10 @@ class Entity < ApplicationRecord
     has_many :manifests
     has_many :contacts
     has_many :conversations, through: :messages
+
+    after_create :generate_manifest
+
+    def generate_manifest
+        Manifest.generate(entity: self)
+    end
 end
--- app/models/manifest.rb
+++ app/models/manifest.rb
@@ -1,2 +1,7 @@
 class Manifest < ApplicationRecord
+    belongs_to :entity
+
+    def self.generate(entity:)
+        #TODO: create a PORO for creating a unique polygon
+    end
 end
