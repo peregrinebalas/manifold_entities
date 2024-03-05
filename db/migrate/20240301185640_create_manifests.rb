f809e8d1b591bb1ec45a0976df814360e6a82073
Peregrine Balas <peregrinereedbalas@gmail.com>
2024-03-04 21:48:48 -0600

    update migrations/models

--- db/migrate/20240301185640_create_manifests.rb
+++ db/migrate/20240301185640_create_manifests.rb
@@ -2,9 +2,10 @@ class CreateManifests < ActiveRecord::Migration[7.1]
   def change
     create_table :manifests do |t|
       t.references :entity, null: false, foreign_key: true
-      t.string :line_color, null: false
-      t.string :fill_color
-      t.vector :embedding, limit: 1536, null: false
+      t.string :stroke, null: false
+      t.string :stroke_width
+      t.string :fill
+      t.vector :polygon, limit: 1536, null: false
 
       t.timestamps
     end
