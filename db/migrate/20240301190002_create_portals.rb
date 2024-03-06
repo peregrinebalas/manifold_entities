d7e9519f7b5863658927a1616f2b79513a9558d0
Peregrine Balas <peregrinereedbalas@gmail.com>
2024-03-05 22:02:19 -0600

    missed a change

--- db/migrate/20240301190002_create_portals.rb
+++ db/migrate/20240301190002_create_portals.rb
@@ -2,7 +2,7 @@ class CreatePortals < ActiveRecord::Migration[7.1]
   def change
     create_table :portals do |t|
       t.boolean :public
-      t.references :user, null: true, foreign_key: true
+      t.references :outsider, null: true, foreign_key: true
       t.st_point :coordinates, geographic: true # longitude, latitude
       t.timestamps
     end
