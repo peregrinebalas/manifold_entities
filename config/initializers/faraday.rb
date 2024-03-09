9b15eaa10d22a79c7db7f03b721a3e533e5239d3
Peregrine Balas <peregrinereedbalas@gmail.com>
2024-03-08 21:49:29 -0600

    initializer

--- config/initializers/faraday.rb
+++ config/initializers/faraday.rb
@@ -1,6 +1,21 @@
 require 'faraday'
 
+class DefaultHeadersMiddleware < Faraday::Middleware
+    def initialize(app, options = {})
+      super(app)
+      @options = options
+    end
+  
+    def call(env)
+      env.request_headers.merge!(@options[:headers]) if @options[:headers]
+      @app.call(env)
+    end
+  end
+
 Faraday.new do |faraday|
-    faraday.headers['Authorization'] = 'Bearer your_api_key'
+    faraday.use DefaultHeadersMiddleware, headers: {
+        'Content-Type' => 'application/json',
+        'Authorization' => 'Bearer YOUR_ACCESS_TOKEN'
+    }
     faraday.adapter Faraday.default_adapter
 end
