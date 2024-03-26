4569d26b0684ff17d569537a27d8e7da1f707df2
Peregrine Balas <peregrinereedbalas@gmail.com>
2024-03-25 19:27:55 -0500

    model updates

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -3,11 +3,7 @@ class Entity < ApplicationRecord
     has_many :manifests, dependent: :delete_all
     has_many :contacts
     has_many :messages
-    has_many :conversations#, through: :messages
+    has_many :conversations
 
-    after_create :generate_manifest
-
-    def generate_manifest
-        Manifest.generate(entity: self)
-    end
+    after_create -> { Manifest.generate(entity: self) }
 end
--- app/models/outsider.rb
+++ app/models/outsider.rb
@@ -1,7 +1,7 @@
 class Outsider < ApplicationRecord
     has_one :entity
-    has_many :messages, through: :entity
-    has_many :conversations#, through: :messages
     has_many :contacts, through: :entity
+    has_many :messages, through: :entity
+    has_many :conversations
     accepts_nested_attributes_for :entity
 end
